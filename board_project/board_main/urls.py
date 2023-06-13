b22cc8ca04b8a516d6c98486a1379d7e0eab13c5
Jeeunun <hanjieun7290@gmail.com>
2023-06-13 15:30:23 +0900

    .

--- board_project/board_main/urls.py
+++ board_project/board_main/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
     path('', views_test.test_html_multi_data),
     path('test_json', views_test.test_json_data),
     #url에 test_json로 들어가면 views_test파일의 test_json_data를 입력해라    
-    path('parameter_data', views_test.test_html_parameter_data)
-     #url에 parameter_data로 들어가면 views_test파일의 test_json_data를 입력해라         
+    path('parameter_data', views_test.test_html_parameter_data),
+    #url에 parameter_data로 들어가면 views_test파일의 parameter_data를 입력해라 
+    path('parameter_data2/<int:my_id>', views_test.test_html_parameter_data2),
+    #url에 parameter_data2로 들어가면 views_test파일의 test_html_parameter_data2를 입력해라 
+    path('test_post', views_test.test_post_form)
+    #url에 test_post로 들어가면 views_test파일의 test_post_form를 입력해라         
 ]
--- board_project/board_main/views_test.py
+++ board_project/board_main/views_test.py
@@ -33,13 +33,36 @@ def test_json_data(request):
 # 사용자가 get요청으로 쿼리파라미터 방식 데이터를 넣어올 때
 # 사용자가 get요청으로 데이터를 넣어오는 방식 2가지
 # 1) 쿼리파라미터 방식 : localhost:8000/author?id=10&name=hongildong 과 같은 방식
-# 2) pathvariable 방식(좀 더 현대적인 방식): localhost:8000/author/10 과 같은 방식
+
 def test_html_parameter_data(request):
-    id = request.GET.get('id')
+    id = request.GET.get('id')   
     name = request.GET.get('name')
-    print(id)
-    print(name)
+
+    # print(id)
+    # print(name)
     #사용자가 request요청 -> request head + request body made by 웹브라우저 
     #       -> urls.py -> views_test.py -> 화면
     # request를 get요청-> id와 name을 get 하겠다. 
     return render(request, 'test/test.html', {})
+
+# 실습 : name, email, password 받아서 화면에 dict형태로
+def test_html_parameter_data(request):
+    name = request.GET.get('name')   
+    email = request.GET.get('email')
+    password = request.GET.get('password')  
+    data = {
+        'name': name ,
+        'email': email,
+        'password': password
+    }
+    return render(request, 'test/test.html', {"my_data": data})   
+
+# 2) pathvariable 방식(좀 더 현대적인 방식): localhost:8000/author/10 과 같은 방식
+def test_html_parameter_data2(request, my_id):
+    print(my_id)
+    return render(request, 'test/test.html', {})   
+
+# form태그를 활용한 post방식
+# 먼저 화면을 rendering해주는 method
+def test_post_form(request):
+    return render(request, 'test/test_post_form.html')
