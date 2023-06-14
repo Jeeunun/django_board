8e171b939ce5b5f5e8d361087def73df6a098981
Jeeunun <hanjieun7290@gmail.com>
2023-06-14 13:30:31 +0900

    .

--- board_project/board_main/urls.py
+++ board_project/board_main/urls.py
@@ -11,7 +11,10 @@ urlpatterns = [
     #url에 parameter_data로 들어가면 views_test파일의 parameter_data를 입력해라 
     path('parameter_data2/<int:my_id>', views_test.test_html_parameter_data2),
     #url에 parameter_data2로 들어가면 views_test파일의 test_html_parameter_data2를 입력해라 
-    path('test_post_handle', views_test.test_post_handle),   
-          
+    path('test_post_handle', views_test.test_post_handle),
+    path('test_select_one/<int:my_id>', views_test.test_select_one), 
+    path('test_select_all', views_test.test_select_all),    
+    path('test_select_multi', views_test.test_select_multi),
+    path('test_update', views_test.test_update),            
 ]
 
--- board_project/board_main/views_test.py
+++ board_project/board_main/views_test.py
@@ -68,7 +68,10 @@ def test_html_parameter_data2(request, my_id):
 # def test_post_form(request):
 #     return render(request, 'test/test_post_form.html')
 
-
+# test_post_handle url에 request가 post형식으로 들어오면 if 문 
+# ->"http://localhost:8000/test_post_handle"에 request save.
+# / post형식이 아니면(get요청) else문 
+# -> test_post_form.html의 "http://localhost:8000/test_post_handle"(홈화면)로 이동
 def test_post_handle(request):
     if request.method == 'POST':
         my_name = request.POST['my_name']
@@ -84,3 +87,50 @@ def test_post_handle(request):
         return  redirect('/') #home으로 가라 => localhost:8000/
     else:
         return render(request, 'test/test_post_form.html')
+    
+
+def test_select_one(request, my_id):
+    # 단 건 만을 조회할 때 get() 함수 사용.
+    t1 = Test.objects.get(id=my_id)
+    # test class의 object를 get()   
+    return render(request, 'test/test_select_one.html', {'data' : t1})
+
+
+def test_select_all(request):
+    # 모든 data조회 시 select * from XXXX: all() 함수 사용
+    tests = Test.objects.all
+    # 타입을 호출하고 값을 찍어봐야한다. 
+    # for a in tests:
+    #     print(a.name) #dict가 아니라 객체이기 때문에 a['name']이 아니라 a.name으로 !
+    #     print(a.email)
+    #    print(a.password)
+    #-> for 문의 실행 그대로 test/test_select_all.html 실행된다. 
+    return render(request, 'test/test_select_all.html',{'datas':tests})
+    # datas에 tests를 담아 test_select_all.html 화면에 response
+
+# where조건으로 다건을 조회할 땐 filter()함수 사용.
+def test_select_multi(request):
+    my_name = request.GET.get('name')
+    t2 = Test.objects.filter(name = my_name) 
+    return render(request, 'test/test_select_multi.html', {'datas': t2})   
+# request시 url "localhost:8000/test_select_multi" 에서 
+# test_select_multi method의 데이터를 response 
+# 데이터? => filter
+
+# update를 하기 위해서는 해당 건을 사전에 조회하기 위한 id값이 필요하다.
+# 메서드는 등록과 동일하게 save()함수 사용
+def test_update(request):
+    if request.method == 'POST':
+        my_id = request.POST['my_id']
+        my_name = request.POST['my_name']
+        my_email = request.POST['my_email']
+        my_password = request.POST['my_password']
+        print(type(my_id))
+        # t1 = Test()
+        # t1.name = my_name
+        # t1.email = my_email
+        # t1.password = my_password
+        # t1.save()
+        return  redirect('/') 
+    else:
+        return render(request, 'test/test_update.html')
